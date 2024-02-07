09742983bb900fe3e0813bebf5f6d510a54779b2
BDFFZI <9080422+bdffzi@user.noreply.gitee.com>
2024-02-07 20:27:34 +0800

    -

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,6 @@
 
 // 取消提供源地址，解决防盗链问题 <https://www.jianshu.com/p/5b58ecce6443>
 hexo.extend.injector.register("head_end", '<meta name="referrer" content="no-referrer" />');
-// 增加评论系统
-hexo.extend.injector.register("body_end",)
-
 // 自动调整markdown的图片地址
 hexo.extend.filter.register("before_post_render", require("./autoAsset"));
 // 根据一级标题自动设置名称
